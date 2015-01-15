66475a796e7c2bcc60d3e6824e756dc390d08452
Gideon Wulfsohn <gideon.wulfsohn@gmail.com>
2015-01-15 13:36:12 -0800

    speed test

--- exercises/cuda/simple_stencil/kernel.cu
+++ exercises/cuda/simple_stencil/kernel.cu
@@ -29,7 +29,7 @@
 #endif
 
 #define N ( 1024 * 1024 )
-#define RADIUS 5
+#define RADIUS 7
 #define THREADS_PER_BLOCK 512
 
 __global__ void stencil_1d(int n, double *in, double *out)
@@ -37,7 +37,7 @@ __global__ void stencil_1d(int n, double *in, double *out)
 /* calculate global index in the array */
 /* insert code to calculate global index in the array using block
    and thread built-in variables */
-  int globalIndex = FIXME
+  int globalIndex = blockIdx.x * blockDim.x + threadIdx.x;
 	
 /* return if my global index is larger than the array size */
   if( globalIndex >= n ) return;
@@ -55,7 +55,7 @@ __global__ void stencil_1d(int n, double *in, double *out)
   {
 /* add the required elements from the array "in" to the temporary 
    variable "result */
-    result += FIXME
+    result += in[i];
   }
 
   out[globalIndex] = result;
@@ -94,7 +94,7 @@ int main()
 
   dim3 threads( THREADS_PER_BLOCK, 1, 1);
 /* insert code for proper number of blocks in X dimension */
-  dim3 blocks( FIXME, 1, 1);
+  dim3 blocks( (N / threads.x) +1, 1, 1);
 
 /* start the timers */
 
